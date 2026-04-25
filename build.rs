58eb62d2bcd8a5984bb25d53b21658bce18a9108
kardelitaitu <adikaradwiatmaja@gmail.com>
2026-04-25 15:22:52 +0700

    chore: remove Windows icon resource wiring (simplify build)

--- build.rs
+++ build.rs
@@ -1,10 +1,1 @@
-fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("icon.ico");
-        if let Err(e) = res.compile() {
-            eprintln!("Failed to compile Windows resources: {}", e);
-        }
-    }
-}
+fn main() {}
